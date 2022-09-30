a18e03454e69ea24678e47122505c2002c531967
Michel Klein <michel.kps@outlook.com>
2022-09-30 10:56:07 -0300

    feat: does not allow two tasks with same name

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { Alert, StyleSheet, View } from 'react-native';
 
 import { Header } from '../components/Header';
 import { Task, TasksList } from '../components/TasksList';
@@ -9,6 +9,8 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
+    const hasTaskWithSameTitle = tasks.find(task => task.title === newTaskTitle)
+    if (hasTaskWithSameTitle) return Alert.alert("Task já cadastrada", "Você não pode cadastrar uma task com o mesmo nome");
     const newTask: Task = {
       id: new Date().getTime(),
       title: newTaskTitle,
